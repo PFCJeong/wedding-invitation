77b8cf55d4113052da2f8423493f67ec692d9f3b
wonsik <lgock2023@gmail.com>
2026-01-09 09:11:42 +0900

    4,7 이미지 제거

--- src/components/Gallery/photo.js
+++ src/components/Gallery/photo.js
@@ -1,10 +1,8 @@
 import image1 from '../../assets/photo/1.webp';
 import image2 from '../../assets/photo/2.webp';
 import image3 from '../../assets/photo/3.webp';
-import image4 from '../../assets/photo/4.webp';
 import image5 from '../../assets/photo/5.webp';
 import image6 from '../../assets/photo/6.webp';
-import image7 from '../../assets/photo/7.webp';
 import image8 from '../../assets/photo/8.webp';
 import image9 from '../../assets/photo/9.webp';
 import image10 from '../../assets/photo/10.webp';
@@ -25,25 +23,23 @@ const photoList = {
   '1-1': image1,
   '1-2': image2,
   '1-3': image3,
-  '2-1': image4,
-  '2-2': image5,
-  '2-3': image6,
-  '3-1': image7,
-  '3-2': image8,
-  '3-3': image9,
-  '4-1': image10,
-  '4-2': image11,
-  '4-3': image12,
-  '5-1': image13,
-  '5-2': image14,
-  '5-3': image15,
-  '6-1': image16,
-  '6-2': image17,
-  '6-3': image18,
-  '7-1': image19,
-  '7-2': image20,
-  '7-3': image21,
-  '8-1': image22,
+  '2-1': image5,
+  '2-2': image6,
+  '2-3': image8,
+  '3-1': image9,
+  '3-2': image10,
+  '3-3': image11,
+  '4-1': image12,
+  '4-2': image13,
+  '4-3': image14,
+  '5-1': image15,
+  '5-2': image16,
+  '5-3': image17,
+  '6-1': image18,
+  '6-2': image19,
+  '6-3': image20,
+  '7-1': image21,
+  '7-2': image22,
 };
 
 export default photoList;
